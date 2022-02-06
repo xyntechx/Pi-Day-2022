6c8da391c1b5ebb95d036f6e65c50ba31c25c0ef
xyntechx <xyntechx@gmail.com>
2022-02-06 10:30:54 +0800

    ✨ Days buttons on homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,4 @@
-import Five from "~/components/five";
-import Four from "~/components/four";
-import Joke from "~/components/joke";
-import One from "~/components/one";
-import Six from "~/components/six";
-import Three from "~/components/three";
-import Two from "~/components/two";
+import Days from "~/components/days";
 import styles from "../styles/styles.css";
 
 export function links() {
@@ -14,79 +8,16 @@ export function links() {
 export default function Index() {
     const currDate = new Date();
     const dayOneDate = new Date(2022, 2, 8);
-    const dayTwoDate = new Date(2022, 2, 9);
-    const dayThreeDate = new Date(2022, 2, 10);
-    const dayFourDate = new Date(2022, 2, 11);
-    const dayFiveDate = new Date(2022, 2, 12);
-    const daySixDate = new Date(2022, 2, 13);
-    const jokeDate = new Date(2022, 2, 14);
+    // const dayOneDate = new Date(2022, 0, 31);
 
-    switch (currDate.toDateString()) {
-        case dayOneDate.toDateString():
-            return <One />;
-        case dayTwoDate.toDateString():
-            return (
-                <>
-                    <One />
-                    <Two />
-                </>
-            );
-        case dayThreeDate.toDateString():
-            return (
-                <>
-                    <One />
-                    <Two />
-                    <Three />
-                </>
-            );
-        case dayFourDate.toDateString():
-            return (
-                <>
-                    <One />
-                    <Two />
-                    <Three />
-                    <Four />
-                </>
-            );
-        case dayFiveDate.toDateString():
-            return (
-                <>
-                    <One />
-                    <Two />
-                    <Three />
-                    <Four />
-                    <Five />
-                </>
-            );
-        case daySixDate.toDateString():
-            return (
-                <>
-                    <One />
-                    <Two />
-                    <Three />
-                    <Four />
-                    <Five />
-                    <Six />
-                </>
-            );
-        case jokeDate.toDateString():
-            return (
-                <>
-                    <One />
-                    <Two />
-                    <Three />
-                    <Four />
-                    <Five />
-                    <Six />
-                    <Joke />
-                </>
-            );
-        default:
-            return (
-                <main className="main">
-                    <h1 className="title">Pi Day 🥧</h1>
-                    <p className="text">Wait till March 8, 2022!</p>
-                </main>
-            );
-    }
+    return (
+        <main className="main">
+            <h1 className="title">Pi Day 🥧</h1>
+            {currDate.getTime() < dayOneDate.getTime() ? (
+                <p className="text">Come back on March 8, 2022!</p>
+            ) : (
+                <Days />
+            )}
+        </main>
+    );
 }

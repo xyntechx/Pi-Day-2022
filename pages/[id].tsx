b12ed5cbbcd35f38088a6bd456df8665c98fc60f
xyntechx <xyntechx@gmail.com>
2022-02-26 15:46:30 +0800

    ✨ Q2-3

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -13,7 +13,9 @@ const Quiz: NextPage = () => {
         case "1":
             return <Q1 />;
         case "2":
-        // return <Q2 />;
+            return <Q2 />;
+        case "2":
+            return <Q3 />;
         default:
             return <h1>hi?</h1>;
     }
@@ -68,6 +70,115 @@ const Q1: NextPage = () => {
             {correct ? (
                 <>
                     <p className={styles.description}>Correct!</p>
+                    <p className={styles.description}>Nice and easy 😁</p>
+                    <br />
+                    <Link href="/2">
+                        <a className={styles.button}>Next Question</a>
+                    </Link>
+                </>
+            ) : (
+                <>
+                    {answered ? (
+                        <p className={styles.description}>
+                            Oops! That&apos;s not right...
+                        </p>
+                    ) : (
+                        <></>
+                    )}
+                </>
+            )}
+        </main>
+    );
+};
+
+const Q2: NextPage = () => {
+    const [answer, setAnswer] = useState("");
+    const [answered, setAnswered] = useState(false);
+    const [correct, setCorrect] = useState(false);
+    const CORRECT = process.env.NEXT_PUBLIC_Q2;
+
+    const checkAnswer = () => {
+        setAnswered(true);
+        if (answer === CORRECT) setCorrect(true);
+    };
+
+    return (
+        <main className={styles.main}>
+            <Meta />
+            <h1 className={styles.title}>
+                Solve for x, where x = 162*172839506
+            </h1>
+            <input
+                type="text"
+                placeholder="My Answer"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                className={styles.input}
+                onFocus={() => setAnswered(false)}
+            />
+            <button className={styles.button} onClick={() => checkAnswer()}>
+                Check My Answer
+            </button>
+            <br />
+            {correct ? (
+                <>
+                    <p className={styles.description}>Correct!</p>
+                    <p className={styles.description}>
+                        Fun fact: it&apos; a palindrome!
+                    </p>
+                    <br />
+                    <Link href="/2">
+                        <a className={styles.button}>Next Question</a>
+                    </Link>
+                </>
+            ) : (
+                <>
+                    {answered ? (
+                        <p className={styles.description}>
+                            Oops! That&apos;s not right...
+                        </p>
+                    ) : (
+                        <></>
+                    )}
+                </>
+            )}
+        </main>
+    );
+};
+
+const Q3: NextPage = () => {
+    const [answer, setAnswer] = useState("");
+    const [answered, setAnswered] = useState(false);
+    const [correct, setCorrect] = useState(false);
+    const CORRECT = process.env.NEXT_PUBLIC_Q3;
+
+    const checkAnswer = () => {
+        setAnswered(true);
+        if (answer === CORRECT) setCorrect(true);
+    };
+
+    return (
+        <main className={styles.main}>
+            <Meta />
+            <h1 className={styles.title}>Solve 8/2(2+2)</h1>
+            <input
+                type="text"
+                placeholder="My Answer"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                className={styles.input}
+                onFocus={() => setAnswered(false)}
+            />
+            <button className={styles.button} onClick={() => checkAnswer()}>
+                Check My Answer
+            </button>
+            <br />
+            {correct ? (
+                <>
+                    <p className={styles.description}>Correct!</p>
+                    <p className={styles.description}>
+                        8/2 = 4, then 2+2 = 4, finally 4*4 = 16
+                    </p>
                     <br />
                     <Link href="/2">
                         <a className={styles.button}>Next Question</a>

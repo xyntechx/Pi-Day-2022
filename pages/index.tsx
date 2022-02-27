7b7052d1bae2441b6a9004f5799ab0613bd56c1e
xyntechx <xyntechx@gmail.com>
2022-02-27 10:09:24 +0800

    🐛 Date set back to 8/3

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
     const currDate = new Date();
-    // const dayOneDate = new Date(2022, 2, 8);
-    const dayOneDate = new Date(2022, 0, 31);
+    const dayOneDate = new Date(2022, 2, 8);
+    // const dayOneDate = new Date(2022, 0, 31);
 
     return (
         <main className={styles.main}>

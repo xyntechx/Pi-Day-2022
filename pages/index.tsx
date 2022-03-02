0f0ffb8ea2644cb8fffb2d9d9b2d0901d0acb5e0
xyntechx <xyntechx@gmail.com>
2022-03-02 11:23:07 +0800

    ✨ Access Joke code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from "next";
 import Days from "../components/days";
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 const Home: NextPage = () => {
     const currDate = new Date();
     const dayOneDate = new Date(2022, 2, 8);
-    // const dayOneDate = new Date(2022, 0, 31);
+    const jokeDate = new Date(2022, 2, 14);
+
+    const [answer, setAnswer] = useState("");
+    const [correct, setCorrect] = useState(false);
+
+    const checkAnswer = () => {
+        if (answer === process.env.NEXT_PUBLIC_KEY) setCorrect(true);
+    };
 
     return (
         <main className={styles.main}>
@@ -32,10 +40,44 @@ const Home: NextPage = () => {
             {currDate.getTime() < dayOneDate.getTime() ? (
                 <>
                     <h1 className={styles.title}>Pi Day 🥧</h1>
-                    <p className={styles.subtitle}>Come back on March 8, 2022!</p>
+                    <p className={styles.subtitle}>
+                        Come back on March 8, 2022!
+                    </p>
                 </>
             ) : (
-                <Days />
+                <>
+                    <Days />
+                    <br />
+                    {currDate.getTime() >= jokeDate.getTime() ? (
+                        <>
+                            <input
+                                type="text"
+                                placeholder="The Code (26 Letters)..."
+                                value={answer}
+                                onChange={(e) => setAnswer(e.target.value)}
+                                className={styles.input}
+                            />
+                            <button
+                                className={styles.button}
+                                onClick={() => checkAnswer()}
+                            >
+                                Check My Answer
+                            </button>
+                            <br />
+                            {correct ? (
+                                <Link href={"/joke"}>
+                                    <a className={styles.button}>
+                                        Give me the Joke
+                                    </a>
+                                </Link>
+                            ) : (
+                                <></>
+                            )}
+                        </>
+                    ) : (
+                        <></>
+                    )}
+                </>
             )}
         </main>
     );
